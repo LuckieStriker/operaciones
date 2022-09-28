3f9d1c43e98c6707044123ef901c7c2c5eadc1de
Miguel Moran <miguelrmor@gmail.com>
2022-09-28 18:38:27 -0500

    feat: avance de calculadora

---
+++ calculadora.js
@@ -0,0 +1,12 @@
+
+const suma = (a,b)=> a+b;
+const resta= (a,b)=> a-b;
+const mult= (a,b)=> a*b;
+const divi= (a,b)=> a/b;
+
+switch (calc){
+    case "+": (suma);
+    case "-": (resta);
+    case "*": (mult);
+    case "/": (divi);
+}
